5f067ca60820755ea87e7cc2a2e7a6b4e0115aee
mxmklvrv <maximkleverov@icloud.com>
2025-04-16 12:36:40 +0300

    strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/16 11:21:18 by mklevero          #+#    #+#             */
+/*   Updated: 2025/04/16 11:21:18 by mklevero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t ft_strlcpy(char *dst, const char *src, size_t n)
+{
+    size_t  i;
+    size_t  j;
+
+    i = 0;
+    j = 0;
+
+    if(n <= 0)
+        return(ft_strlen(src));
+    
+    while(src[i])
+    {
+        if(i < (n - 1))
+        {
+            dst[i] = src[i];
+            j++;
+        }
+        i++;
+    }
+    dst[j] = '\0';
+    return (i);
+}
+/*
+int main(void)
+{
+    char src[] = "odin dva tri";
+    char dst[7];
+    size_t len;
+
+    len =  ft_strlcpy(dst, src, sizeof(dst));
+    printf(%d, len);
+
+}
+*/
