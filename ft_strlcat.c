639ef5aa5010389c07795f1941d4820dceb61bc0
mxmklvrv <maximkleverov@icloud.com>
2025-04-16 13:40:00 +0300

    strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/16 12:47:50 by mklevero          #+#    #+#             */
+/*   Updated: 2025/04/16 12:47:50 by mklevero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t ft_strlcat(char *dst, const char *src, size_t d_size)
+{
+    size_t i; //length of dest
+    size_t j; 
+
+    i = 0;
+    j = 0;
+
+    while(dst[i] && i < d_size) 
+        i++; 
+    if(i >= d_size)
+        return(d_size + ft_strlen(src));
+    while(src[j] && ((i + j + 1) < d_size))
+    {
+        dst[i + j] = src[j];
+        j++;
+    }
+    dst[i + j] = '\0';
+    return(i + ft_strlen(src));
+}
+
+/*
+int main(void)
+{
+    char dst[15] = "odin";
+    char *src = "dva";
+    size_t res = ft_strlcat(dst, src, sizeof(dst));
+
+    printf("%zu", res);
+
+}*/
