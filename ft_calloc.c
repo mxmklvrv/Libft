1df80d6a72a8cfeb8c134dcf1b2baa06ddc72b49
mxmklvrv <maximkleverov@icloud.com>
2025-04-19 14:25:57 +0300

    calloc (need an overflow check)

---
+++ ft_calloc.c
@@ -0,0 +1,15 @@
+
+/*allocates memory, count - numbr of elements to allocate, size - size of each element */
+#include "libft.h"
+
+void    *ft_calloc(size_t count, size_t size)
+{
+    void *ptr;
+    /* TODO - OVERFLOW CHECK*/
+    ptr = malloc(count * size);
+    if (ptr == NULL)
+        return (NULL);
+    ft_memset(ptr, 0, (count * size));
+    return (ptr);
+    
+}
