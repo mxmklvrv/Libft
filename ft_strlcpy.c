67a9f1339e77df089b7345e187a2a3d7af2ec2b3
mxmklvrv <maximkleverov@icloud.com>
2025-04-22 14:32:22 +0300

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -15,23 +15,18 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t n)
 {
 	size_t	i;
-	size_t	j;
 
 	i = 0;
-	j = 0;
-	if (n <= 0)
-		return (ft_strlen(src));
-	while (src[i])
-	{
-		if (i < (n - 1))
-		{
-			dst[i] = src[i];
-			j++;
-		}
-		i++;
-	}
-	dst[j] = '\0';
-	return (i);
+	if (n > 0)
+    {
+        while (src[i] && i < (n - 1))
+	    {
+		    dst[i] = src[i];
+            i++;
+	    }
+	    dst[i] = '\0';
+    }
+	return (ft_strlen(src));
 }
 /*
 int	main(void)
