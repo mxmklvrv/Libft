3234117cf5c8283a3bc520f5731eb6b5d9c03121
mxmklvrv <maximkleverov@icloud.com>
2025-04-23 14:54:38 +0300

    ft_putnbr_fd

--- ft_putnmbr_fd.c
+++ ft_putnmbr_fd.c
@@ -6,34 +6,44 @@
 /*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 14:52:44 by mklevero          #+#    #+#             */
-/*   Updated: 2025/04/17 14:53:59 by mklevero         ###   ########.fr       */
+/*   Updated: 2025/04/23 14:53:32 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*CONTAINS ORIGINAL FT_PUTNUMMBR. NEEDS TO BE REVISED*/
+/*
 #include <unistd.h>
 
-void	ft_printchar(char c)
+void ft_putchar_fd(char c, int fd)
 {
-	write(1, &c, 1);
+    write(fd, &c, 1);
 }
-
-void	ft_putnbr(int nb)
+*/
+void ft_putnbr_fd(int n, int fd)
 {
-	if (nb == -2147483648)
+	if (n == -2147483648)
 	{
-		ft_printchar('-');
-		ft_printchar('2');
-		nb = 147483648;
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		n = 147483648;
 	}
-	if (nb < 0)
+	if (n < 0)
 	{
-		ft_printchar('-');
-		nb = -nb;
+		ft_putchar_fd('-', fd);
+		n = -n;
 	}
-	if (nb >= 10)
+	if (n >= 10)
 	{
-		ft_putnbr(nb / 10);
+		ft_putnbr_fd(n / 10, fd);
 	}
-	ft_printchar((nb % 10) + '0');
+	ft_putchar_fd(((n % 10) + '0'), fd);
 }
+/*
+int main (void)
+{
+	ft_putnbr_fd(-2147483648, 1);
+	ft_putchar_fd('\n', 1);
+	ft_putnbr_fd(-123456, 1);
+	ft_putchar_fd('\n', 1);
+	ft_putnbr_fd(0, 1);
+}
+*/
