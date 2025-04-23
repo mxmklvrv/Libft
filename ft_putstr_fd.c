bf79521539e3c4021cedc9eda26b42cccdd8e981
mxmklvrv <maximkleverov@icloud.com>
2025-04-23 14:36:07 +0300

    added NULL check

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -1,15 +1,18 @@
 #include "libft.h"
-
+/*
 void ft_putchar_fd(char c, int fd)
 {
     write(fd, &c, 1);
 }
+*/
 /*Outputs the string ’s’ to the specified file descriptor.*/
 void ft_putstr_fd(char *s, int fd)
 {
     int i;
 
     i = 0;
+    if (s == NULL)
+        return ;
     while(s[i])
     {
         ft_putchar_fd(s[i], fd);
@@ -17,9 +20,11 @@ void ft_putstr_fd(char *s, int fd)
     }
 
 }
+/*
 #include <unistd.h>
 int main (void)
 {
     ft_putstr_fd("Korabli lavirovali, lavirovali, da ne vylavirovali\n", 1);
     return (0);
 }
+*/
