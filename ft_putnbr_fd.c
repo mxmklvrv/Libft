a5ae6547e4e33ddc79578c0c2486c949f511bc18
mxmklvrv <maximkleverov@icloud.com>
2025-04-23 14:56:04 +0300

    ft_putnbr_fd

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,38 @@
+
+/*
+#include <unistd.h>
+
+void ft_putchar_fd(char c, int fd)
+{
+    write(fd, &c, 1);
+}
+*/
+void ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		n = 147483648;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+	}
+	ft_putchar_fd(((n % 10) + '0'), fd);
+}
+/*
+int main (void)
+{
+	ft_putnbr_fd(-2147483648, 1);
+	ft_putchar_fd('\n', 1);
+	ft_putnbr_fd(-123456, 1);
+	ft_putchar_fd('\n', 1);
+	ft_putnbr_fd(0, 1);
+}
+*/
