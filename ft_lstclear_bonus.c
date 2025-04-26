53e7f90fcb68d8c86f58d1190e87c10a709663f2
Maxim Kleverov <mklevero@student.hive.fi>
2025-04-26 14:56:42 +0300

    lstclear and lstdelone updated to norm

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 14:53:30 by mklevero          #+#    #+#             */
+/*   Updated: 2025/04/26 14:53:52 by mklevero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list *temp;
+	t_list *current;
+
+	if (lst == NULL || del == NULL)
+		return ;
+	current = *lst;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		del(temp->content);
+		free(temp);
+	}
+	*lst = NULL;
+}
---
+++ ft_lstdelone_bonus.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 14:54:57 by mklevero          #+#    #+#             */
+/*   Updated: 2025/04/26 14:55:44 by mklevero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if(lst == NULL || del == NULL)
+		return ;
+	del(lst->content);
+	free(lst);
+}
