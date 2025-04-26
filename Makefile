a3f98d59c4995ac43da1df34f3ff208b00eca382
Maxim Kleverov <mklevero@student.hive.fi>
2025-04-26 16:27:25 +0300

    updated make and lbft.h

--- Makefile
+++ Makefile
@@ -18,7 +18,13 @@ ft_striteri.c ft_strjoin.c ft_strlcat.c ft_strlcpy.c ft_strlen.c \
 ft_strmapi.c ft_strncmp.c ft_strnstr.c ft_strrchr.c ft_strtrim.c \
 ft_substr.c ft_tolower.c ft_toupper.c 
 
+BONUSSRCS = ft_lstadd_back_bonus.c ft_lstadd_front_bonus.c \
+ft_lstclear_bonus.c ft_lstdelone_bonus.c ft_lstiter_bonus.c \
+ft_lstlast_bonus.c ft_lstmap_bonus.c ft_lstnew_bonus.c ft_lstsize_bonus.c
+
 OBJ = $(SRCS:.c=.o)
+BONUSOBJ = $(BONUSSRCS:.c=.o)
+
 
 NAME = libft.a
 CC = cc
@@ -33,11 +39,14 @@ $(NAME): $(OBJ)
 	$(CC) $(CFLAGS) -c $< -o $@
 
 clean:
-	rm -f $(OBJ)
+	rm -f $(OBJ) $(BONUSOBJ)
 
 fclean: clean
 	rm -f $(NAME)
 re:
 	fclean all
+
+bonus: $(OBJ) $(BONUSOBJ)
+	ar rcs $(NAME) $(OBJ) $(BONUSOBJ)
 
 .PHONY: all clean fclean re
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 18:32:50 by mklevero          #+#    #+#             */
-/*   Updated: 2025/04/25 21:04:48 by mklevero         ###   ########.fr       */
+/*   Updated: 2025/04/26 16:26:42 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,5 +55,15 @@ char				*ft_strtrim(char const *s1, char const *set);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 int					ft_tolower(int c);
 int					ft_toupper(int c);
+
+void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstadd_front(t_list **lst, t_list *new);
+void				ft_lstclear(t_list **lst, void (*del)(void *));
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+t_list				*ft_lstlast(t_list *lst);
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list				*ft_lstnew(void *content);
+int					ft_lstsize(t_list *lst);
 
 #endif
