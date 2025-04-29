1f92d05a5b15333784285c591abe850fb8d58d27
mxmklvrv <maximkleverov@icloud.com>
2025-04-29 13:31:25 +0300

    comment added

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,23 +6,15 @@
 /*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 12:42:09 by mklevero          #+#    #+#             */
-/*   Updated: 2025/04/24 13:34:10 by mklevero         ###   ########.fr       */
+/*   Updated: 2025/04/29 13:31:02 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*write char to specified file descriptor*/
+//write char to specified file descriptor
+
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
-/*
-#include <unistd.h>
-
-int main (void)
-{
-	ft_putchar_fd('M', 1);
-	return (0);
-}
-*/
