4d5386cbb9539e5a182782a31572d421cdf65e0e
mxmklvrv <maximkleverov@icloud.com>
2025-04-29 14:08:33 +0300

    comment added

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,11 +6,15 @@
 /*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 19:21:11 by mklevero          #+#    #+#             */
-/*   Updated: 2025/04/25 21:25:39 by mklevero         ###   ########.fr       */
+/*   Updated: 2025/04/29 14:08:10 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+// Adds the node ’new’ at the beginning of the list.
+// lst: The address of a pointer to the first node of a list.
+// new: The address of a pointer to the node to be added.
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
